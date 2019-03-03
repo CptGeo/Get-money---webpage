6ab6d51e92be5554ce32a4d23f7621ff62d82e3a
George <georgekalyvianakis18@gmail.com>
2019-03-03 02:59:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var cards = document.querySelectorAll('.cards__space');
 var videos = document.querySelectorAll('.background__video');
 
@@ -18,32 +17,3 @@ cards.forEach(item=>{
             video.style.opacity="0";
     })
 })
-
-
-=======
-var cards = document.querySelectorAll('.cards__space');
-var videos = document.querySelectorAll('.background__video');
-
-
-
-cards.forEach(item=>{
-    item.addEventListener('mouseenter',(e)=>{
-        if(e.target.dataset.item){
-            var index = parseInt(e.target.dataset.item);
-            var video=videos[index-1];
-            video.load();
-            video.play();
-            video.style.opacity="1";
-        }
-    })
-    item.addEventListener('mouseleave',(e)=>{
-        if(e.target.dataset.item){
-            var index = parseInt(e.target.dataset.item);
-            var video=videos[index-1];
-            video.style.opacity="0";
-        }
-    })
-})
-
-
->>>>>>> 4519e2d8e12a86db941caf5a0ec7ebc8e4966346
