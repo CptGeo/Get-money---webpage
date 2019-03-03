4d9204ecd48ec1f06521abc831e7135bf19930d2
CptGeo <georgekalyvianakis18@gmail.com>
2019-03-03 02:39:47 +0200

    Home page

---
+++ script.js
@@ -0,0 +1,26 @@
+var cards = document.querySelectorAll('.cards__space');
+var videos = document.querySelectorAll('.background__video');
+var col= document.querySelector('.cards');
+
+
+
+cards.forEach(item=>{
+    item.addEventListener('mouseenter',(e)=>{
+        if(e.target.dataset.item){
+            var index = parseInt(e.target.dataset.item);
+            var video=videos[index-1];
+            video.load();
+            video.play();
+            video.style.opacity="1";
+        }
+    })
+    item.addEventListener('mouseleave',(e)=>{
+        if(e.target.dataset.item){
+            var index = parseInt(e.target.dataset.item);
+            var video=videos[index-1];
+            video.style.opacity="0";
+        }
+    })
+})
+
+
